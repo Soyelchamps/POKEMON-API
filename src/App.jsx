4c6d582e68d26b697845c5d5ad312ee40cf0cdcc
Carlos <carlos@MacBook-Pro-de-Carlos.local>
2022-05-18 21:16:39 -0500

    ultimo comentario

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Layout from "./routes/Layout/layout";
 import Home from "./routes/home/home";
 import User from "./routes/users/user";
@@ -9,12 +10,33 @@ import SearchBox from "./components/search-box/search-box";
 import CardList from "./components/card-list/card-list";
 const App = () => {
   return (
+    //   <>
+    //   <nav>
+    //     <Link to="/">Home</Link>
+    //     <Link to="/about">About</Link>
+    //   </nav>
+    //   <div style={{ display: "flex" }}>
+    //     <div>Sidebar</div>
+    //     <div style={{ marginLeft: "25px" }}>
+    //       <Outlet />
+    //     </div>
+    //   </div>
+    // </>
+    
+    
+    
+    
+    <>
+    <nav>
     <Routes>
       <Route path="/" element={<Home />}>
         <Route path="/pokemon/:name" element={<User />} />
+        
       </Route>
-      <Route index path="/test" element={<About />} />
+      <Route index path="/About" element={<About />} />
     </Routes>
+    </nav>
+    </>
   );
 };
 
--- src/routes/Layout/layout.jsx
+++ src/routes/Layout/layout.jsx
@@ -1,21 +1,18 @@
 import { Fragment } from "react";
 import { Outlet, Link } from "react-router-dom";
-
-
 import CrwnLogo from "../../assets/crown.svg?component";
 
 const Layout = () => {
   return (
     <Fragment>
-      <CrwnLogo />
-      <nav>
+      <nav > 
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
       </nav>
       <div style={{ display: "flex" }}>
         <div>Sidebar</div>
         <div style={{ marginLeft: "20px" }}>
-          <Outlet />
+        
         </div>
       </div>
     </Fragment>
